f360b88b228a49c7cc0489485e2e65d4f8ca1ba3
Your Name <minajmiha02@gmail.com>
2025-05-13 20:04:46 +0600

    router setup

--- src/router/roter.jsx
+++ src/router/roter.jsx
@@ -1,10 +1,19 @@
 import {
   createBrowserRouter,
 } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import MainLayout from "../Layout/MainLayout";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element:<MainLayout></MainLayout>,
+    errorElement:<h1>Route not found</h1>,
+     children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+    ],
   },
 ]);
 export default router
