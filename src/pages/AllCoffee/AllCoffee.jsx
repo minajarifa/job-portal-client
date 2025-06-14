d796d066dc881a31a86b4ebf7f9c75c7cc341e18
Your Name <minajmiha02@gmail.com>
2025-06-14 19:21:34 +0600

    react icons addedd

--- src/pages/AllCoffee/AllCoffee.jsx
+++ src/pages/AllCoffee/AllCoffee.jsx
@@ -1,3 +1,7 @@
+import { MdDelete } from "react-icons/md";
+import { MdOutlinePreview } from "react-icons/md";
+import { MdOutlineSecurityUpdateGood } from "react-icons/md";
+
 import { useState } from "react";
 
 export default function AllCoffee() {
@@ -11,15 +15,15 @@ export default function AllCoffee() {
   return (
     <div>
       <h1>All Coffee: {coffees?.length}</h1>
-      <div className="grid grid-cols-3 gap-6 ">
+      <div className="grid grid-cols-2 gap-6 ">
         {coffees?.map((coffee) => (
           <div key={coffee?._id}>
             <div className="shadow-sm card card-side bg-base-100">
               <figure>
                 <img src={coffee?.photo} alt="Movie" />
               </figure>
-              <div className="flex card-body ">
-                <div>
+              <div className="bg-red-400 card-body">
+                <div className="">
                   <h2 className="card-title">{coffee?.name}</h2>
                   <p>{coffee?.details}</p>
                   <p>{coffee?.category}</p>
@@ -27,10 +31,15 @@ export default function AllCoffee() {
                   <p>{coffee?.supplier}</p>
                   <p>{coffee?.taste}</p>
                 </div>
-                <div className=" card-actions">
-                  <button className="btn btn-primary">view</button>
-                  <button className="btn btn-primary">delete</button>
-                  <button className="btn btn-primary">update</button>
+                <div className="grid card-actions bg-slate-500">
+                  <button className="btn btn-primary "title="view to click">
+                  <MdOutlinePreview />
+\ 
+                  </button>
+                  <button className="btn btn-primary"><MdDelete />
+</button>
+                  <button className="btn btn-primary"><MdOutlineSecurityUpdateGood />
+</button>
                 </div>
               </div>
             </div>
