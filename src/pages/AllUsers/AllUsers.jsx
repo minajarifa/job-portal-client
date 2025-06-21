700070d25fa9965a2d33d3091607a21e1a17c25d
Your Name <minajmiha02@gmail.com>
2025-06-21 17:12:39 +0600

    update users

--- src/pages/AllUsers/AllUsers.jsx
+++ src/pages/AllUsers/AllUsers.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
+import { IoMdCheckmark } from "react-icons/io";
+import { RiDeleteBin5Fill } from "react-icons/ri";
+import { MdUpdate } from "react-icons/md";
+import Swal from "sweetalert2";
 
 export default function AllUsers() {
   const [users, setUsers] = useState([]);
@@ -11,8 +15,46 @@ export default function AllUsers() {
       });
   }, []);
   console.log(users);
+  // handleDeleteButton operation
+  const handleDeleteButton = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // star delete
+        fetch(`http://localhost:5000/user/${id}`, {
+          method: "DELETE",
+          headers: {
+            "content-type": "application/json",
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount === 1) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+        //  ennd delete
+      }
+    });
+  };
+  // handleUpdateButton operation
+  const handleUpdateButton=(id)=>{
+    console.log(id)
+  }
   return (
-    <div>
+    <div className="my-10">
+      <h1 className="mb-5 text-4xl text-center">Users : {users?.length}</h1>
       {/* start */}
       <section className="container px-4 mx-auto">
         <div className="flex flex-col">
@@ -24,51 +66,9 @@ export default function AllUsers() {
                     <tr>
                       <th
                         scope="col"
-                        className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
-                      >
-                        <div className="flex items-center gap-x-3">
-                          <input
-                            type="checkbox"
-                            className="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700"
-                          />
-
-                          <button className="flex items-center gap-x-2" />
-                          <span>Invoice</span>
-
-                          <svg
-                            className="h-3"
-                            viewBox="0 0 10 11"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M2.13347 0.0999756H2.98516L5.01902 4.79058H3.86226L3.45549 3.79907H1.63772L1.24366 4.79058H0.0996094L2.13347 0.0999756ZM2.54025 1.46012L1.96822 2.92196H3.11227L2.54025 1.46012Z"
-                              fill="currentColor"
-                              stroke="currentColor"
-                              stroke-width="0.1"
-                            />
-                            <path
-                              d="M0.722656 9.60832L3.09974 6.78633H0.811638V5.87109H4.35819V6.78633L2.01925 9.60832H4.43446V10.5617H0.722656V9.60832Z"
-                              fill="currentColor"
-                              stroke="currentColor"
-                              stroke-width="0.1"
-                            />
-                            <path
-                              d="M8.45558 7.25664V7.40664H8.60558H9.66065C9.72481 7.40664 9.74667 7.42274 9.75141 7.42691C9.75148 7.42808 9.75146 7.42993 9.75116 7.43262C9.75001 7.44265 9.74458 7.46304 9.72525 7.49314C9.72522 7.4932 9.72518 7.49326 9.72514 7.49332L7.86959 10.3529L7.86924 10.3534C7.83227 10.4109 7.79863 10.418 7.78568 10.418C7.77272 10.418 7.73908 10.4109 7.70211 10.3534L7.70177 10.3529L5.84621 7.49332C5.84617 7.49325 5.84612 7.49318 5.84608 7.49311C5.82677 7.46302 5.82135 7.44264 5.8202 7.43262C5.81989 7.42993 5.81987 7.42808 5.81994 7.42691C5.82469 7.42274 5.84655 7.40664 5.91071 7.40664H6.96578H7.11578V7.25664V0.633865C7.11578 0.42434 7.29014 0.249976 7.49967 0.249976H8.07169C8.28121 0.249976 8.45558 0.42434 8.45558 0.633865V7.25664Z"
-                              fill="currentColor"
-                              stroke="currentColor"
-                              stroke-width="0.3"
-                            />
-                          </svg>
-                          <button />
-                        </div>
-                      </th>
-
-                      <th
-                        scope="col"
                         className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
                       >
-                        Date
+                        Registers Name
                       </th>
 
                       <th
@@ -82,14 +82,14 @@ export default function AllUsers() {
                         scope="col"
                         className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
                       >
-                        Customer
+                        Users
                       </th>
 
                       <th
                         scope="col"
                         className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
                       >
-                        Purchase
+                        Email
                       </th>
 
                       <th scope="col" className="relative py-3.5 px-4">
@@ -98,81 +98,58 @@ export default function AllUsers() {
                     </tr>
                   </thead>
                   <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-                    <tr>
-                      <td className="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
-                        <div className="inline-flex items-center gap-x-3">
-                          <input
-                            type="checkbox"
-                            className="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700"
-                          />
-
-                          <span>#3066</span>
-                        </div>
-                      </td>
-                      <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                        Jan 6, 2022
-                      </td>
-                      <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                        <div className="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-emerald-500 bg-emerald-100/60 dark:bg-gray-800">
-                          <svg
-                            width="12"
-                            height="12"
-                            viewBox="0 0 12 12"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M10 3L4.5 8.5L2 6"
-                              stroke="currentColor"
-                              stroke-width="1.5"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                            />
-                          </svg>
-
-                          <h2 className="text-sm font-normal">Paid</h2>
-                        </div>
-                      </td>
-                      <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                        <div className="flex items-center gap-x-2">
-                          <img
-                            className="object-cover w-8 h-8 rounded-full"
-                            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
-                            alt=""
-                          />
-                          <div>
-                            <h2 className="text-sm font-medium text-gray-800 dark:text-white ">
-                              Arthur Melo
-                            </h2>
-                            <p className="text-xs font-normal text-gray-600 dark:text-gray-400">
-                              authurmelo@example.com
-                            </p>
+                    {users?.map((user) => (
+                      <tr key={user?._id}>
+                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                          {user?.name}
+                        </td>
+                        <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
+                          <div className="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-emerald-500 bg-emerald-100/60 dark:bg-gray-800">
+                            <IoMdCheckmark />
+                            <h2 className="text-sm font-normal">Paid</h2>
                           </div>
-                        </div>
-                      </td>
-                      <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
-                        Monthly subscription
-                      </td>
-                      <td className="px-4 py-4 text-sm whitespace-nowrap">
-                        <div className="flex items-center gap-x-6">
-                          <button className="text-gray-500 transition-colors duration-200 dark:hover:text-indigo-500 dark:text-gray-300 hover:text-indigo-500 focus:outline-none">
-                            Archive
-                          </button>
-
-                          <button className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
-                            Download
-                          </button>
-                        </div>
-                      </td>
-                    </tr>                  
+                        </td>
+                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                          <div className="flex items-center gap-x-2">
+                            <img
+                              className="object-cover w-8 h-8 rounded-full"
+                              src={"https://i.ibb.co/mrK7Sjcw/download.png"}
+                              alt="user photo"
+                            />
+                            <div>
+                              <h2 className="text-sm font-medium text-gray-800 dark:text-white ">
+                                {user?.name}
+                              </h2>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+                          {user?.email}
+                        </td>
+                        <td className="px-4 py-4 text-sm whitespace-nowrap">
+                          <div className="flex items-center gap-x-6">
+                            <button
+                              onClick={() => handleDeleteButton(user._id)}
+                              className="text-gray-500 transition-colors duration-200 dark:hover:text-indigo-500 dark:text-gray-300 hover:text-indigo-500 focus:outline-none"
+                            >
+                              <RiDeleteBin5Fill
+                                title="delete"
+                                className="text-xl"
+                              />
+                            </button>
+                            <button onClick={()=>handleUpdateButton(user?._id)} className="text-blue-500 transition-colors duration-200 hover:text-indigo-500 focus:outline-none">
+                              <MdUpdate title="update" className="text-xl" />
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
                   </tbody>
                 </table>
               </div>
             </div>
           </div>
         </div>
-
-     
       </section>
       {/* end */}
     </div>
---
+++ src/pages/AllUsers/Update/Update.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Update() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
