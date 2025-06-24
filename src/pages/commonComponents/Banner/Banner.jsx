92816ae5d96239185884e285806bb2987bb41002
Your Name <minajmiha02@gmail.com>
2025-06-24 16:21:23 +0600

    banner component

--- src/pages/commonComponents/Banner/Banner.jsx
+++ src/pages/commonComponents/Banner/Banner.jsx
@@ -1,7 +1,51 @@
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 export default function Banner() {
   return (
-    <div>Banner</div>
-  )
+    <div className="my-10">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper text-center"
+      >
+        <SwiperSlide className="mb-10">
+          <img
+            className="w-full h-96 rounded-3xl"
+            src="https://i.ibb.co/5gP71d0j/images.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="mb-10">
+          <img
+            className="w-full h-96 rounded-3xl"
+            src="https://i.ibb.co/bj0c28g1/download.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="mb-10">
+          <img
+            className="w-full h-96 rounded-3xl"
+            src="https://i.ibb.co/DP1PjYtV/download.jpg"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
