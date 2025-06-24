7d91966c7f3232c9cc161ab13cc0618d79df36cd
Your Name <minajmiha02@gmail.com>
2025-06-24 10:14:17 +0600

    Bannner component created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Banner from "../commonComponents/Banner/Banner";
 
 
 export default function Home() {
   return (
     <div>
+      <Banner/>
      
         <h1 className="text-7">Show All Users:</h1>
         {/*  */}
